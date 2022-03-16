871f59bdbc9d152ed26097fe5363715224953ef5
Antonio Hauren <antoniohauren@gmail.com>
2022-03-16 11:53:11 -0300

    feat: ensure Signin calls EmailValidator with correct email

--- src/Presentation/controllers/signin/signin.ts
+++ src/Presentation/controllers/signin/signin.ts
@@ -1,17 +1,24 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class SigninController implements Controller {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['email', 'password']
+  private readonly emailValidator: EmailValidator
 
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { email } = httpRequest.body
+    const requiredFields = ['email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
-
+    this.emailValidator.isValid(email)
     return {} as any
   }
 }
