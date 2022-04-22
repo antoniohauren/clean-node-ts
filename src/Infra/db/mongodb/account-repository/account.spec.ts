60e8dc529ec3150ae63e0f69603c1596f03c7563
Antonio Hauren <antoniohauren@gmail.com>
2022-04-21 23:44:09 -0300

    test: ensure update account withdates accesstoken on success

--- src/Infra/db/mongodb/account-repository/account.spec.ts
+++ src/Infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,29 @@ describe('Account Mongodb Repository', () => {
     const account = await sut.loadByEmail('my_mail@mail.com')
     expect(account).toBeFalsy()
   })
+
+  it('Should update account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const accountData = {
+      name: 'my_name',
+      email: 'my_email@mail.com',
+      password: 'my_password'
+    }
+
+    const { value: result } = await accountCollection.findOneAndUpdate(accountData,
+      {
+        $setOnInsert: accountData
+      }, {
+        upsert: true,
+        returnDocument: 'after'
+      })
+
+    const { id, accessToken } = MongoHelper.map(result)
+
+    expect(accessToken).toBeFalsy()
+    await sut.updateAccessToken(id, 'any_token')
+    const account = await accountCollection.findOne()
+    expect(account).toBeTruthy()
+    expect(account?.accessToken).toBe('any_token')
+  })
 })
